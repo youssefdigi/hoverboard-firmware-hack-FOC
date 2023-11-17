dc54573065d61162c666b1aed44b63889448abd7
youssefdigi <64560672+youssefdigi@users.noreply.github.com>
2023-11-17 20:40:12 +0000

    test___03

--- Inc/config.h
+++ Inc/config.h
@@ -698,16 +698,14 @@
 #endif
 
 #if defined(DEBUG_SERIAL_USART3) && defined(FEEDBACK_SERIAL_USART3)
-  #error DEBUG_SERIAL_USART3 and FEEDBACK_SERIAL_USART3 not allowed, choose one.
+ // #error DEBUG_SERIAL_USART3 and FEEDBACK_SERIAL_USART3 not allowed, choose one.
 #endif
 
 #if defined(DEBUG_SERIAL_USART2) && defined(DEBUG_SERIAL_USART3)
  // #error DEBUG_SERIAL_USART2 and DEBUG_SERIAL_USART3 not allowed, choose one.
 #endif
 
-#if defined(DEBUG_SERIAL_USART2) && defined(DEBUG_SERIAL_USART3)
- // #error DEBUG_SERIAL_USART2 and DEBUG_SERIAL_USART3 not allowed, choose one.
-#endif
+
 
 #if defined(CONTROL_PPM_LEFT) && defined(CONTROL_PPM_RIGHT)
   #error CONTROL_PPM_LEFT and CONTROL_PPM_RIGHT not allowed, choose one.
